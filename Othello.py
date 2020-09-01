910082da4b9007dd7fb7e73270f086fa6ea04933
okkokko10 <okko.heinio@gmail.com>
2020-09-01 18:04:01 +0300

    Update Othello.py
    added margins, randomized which move AI takes, added ability to see which tiles are valid and what tiles are affected by a move, added indication of victory...
    Missing:
    Options and reset, undo

--- Othello.py
+++ Othello.py
@@ -5,8 +5,15 @@ class Board:
     x=1
     y=1
     pressed=False
+    updated=True
     directions=[(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)]
-    def __init__(self):
+    winner=0
+    turned=False
+    text=False
+    def __init__(self,AItypes=(0,1),showPossible=False,showConsequence=False):
+        self.showPossible=showPossible
+        self.showConsequence=showConsequence
+        self.AItype=AItypes
         self.grid=[]
         f=[]
         for _ in range(8):
@@ -28,49 +35,74 @@ class Board:
         else:
             return 7
     def setTile(self,x,y,side):
+        self.updated=True
         self.grid[y][x]=side
-        self.canvas.place(x,y,side)
+        #self.canvas.place(x,y,side)
     def printSelf(self):
         for i in self.grid:
             print(i)
     def Update(self):
         
-        if self.turn==2 and self.cannotPlace<2:
-            self.cannotPlace=0
-            f=self.Search(2)
+        if self.turned:
+            if self.text:
+                f=self.Count()
+                print("{} empty, {} white, {} black".format(f[0],f[1],f[2]))
+            self.turned=False
+            self.turn=self.turn%2+1
+            self.updated=True
+        if self.cannotPlace<2:
+            f=self.Search(self.turn)
             if f[0]==0:
-                print("opponent cannot place")
+                if self.text:
+                    if self.turn==1:
+                        a="white"
+                    else:
+                        a="black"
+                    print(a+" cannot place")
                 self.cannotPlace+=1
+                self.turned=True
             else:
-                self.Placement(f[2],f[3],2,f)
-            f=self.Search(1)
-            if f[0]==0:
-                print("you cannot place")
-                self.cannotPlace+=1
-            else:
-                self.turn=self.turn%2+1
-            f=self.Count()
-            print("{} empty, {} green, {} blue".format(f[0],f[1],f[2]))
+                self.cannotPlace=0
+                if self.AItype[self.turn-1]==1:
+                    self.Placement(f[2],f[3],self.turn,f)
+                    self.turned=True
+                elif self.pressed and self.AItype[self.turn-1]==0:
+                    if self.Placement(self.x,self.y,self.turn):
+                        self.turned=True
+                    self.pressed=False
+            
         if self.cannotPlace==2:
             self.cannotPlace=3
             f=self.Count()
+            self.updated=True
             if f[1]<f[2]:
-                print("you lose")
+                if self.text:
+                    print("black wins")
+                self.winner=2
             else:
                 if f[1]==f[2]:
-                    print("draw")
+                    if self.text:
+                        print("draw")
+                    self.winner=7
                 else:
-                    print("you win")
+                    if self.text:
+                        print("white wins")
+                    self.winner=1
 
-        # if self.turn==1:
-        #     f=self.Search(1)
-        #     self.Placement(f[2],f[3],1,f)
-        #     self.turn=self.turn%2+1
-        if self.pressed and self.turn==1:
-            if self.Placement(self.x,self.y,1):
-                self.turn=self.turn%2+1
-            self.pressed=False
-        self.canvas.Update()
+
+
+        if self.updated:
+            self.canvas.Clear()
+            self.Count(True)
+            self.canvas.Grid(self.winner)
+            if self.AItype[self.turn-1]==0 and self.winner==0 and not self.turned:
+                self.canvas.place(self.x,self.y,self.turn,2)
+                if self.showConsequence:
+                    self.ScanDraw(self.x,self.y,self.turn)
+                if self.showPossible:
+                    self.Search(self.turn,True)
+            self.canvas.Update()
+            self.updated=False
     def ValidPlacement(self,x,y,side):
         potential=0
         out=[]
@@ -86,10 +118,19 @@ class Board:
         if scan[0]>0:
             self.setTile(x,y,side)
             for i in range(8):
-                self.ScannotPlace(x,y,self.directions[i],scan[1][i],side)
+                self.ScanPlace(x,y,self.directions[i],scan[1][i],side)
             return True
         else:
             return False
+    def ScanDraw(self,x,y,side):
+        self.updated=True
+        scan=self.ValidPlacement(x,y,side)
+        if scan[0]>0:
+            for i in range(8):
+                if scan[1][i]>0:
+                    for k in range(scan[1][i]):
+                        self.canvas.place(x+self.directions[i][0]*(k+1),y+self.directions[i][1]*(k+1),side,1)
+
     def Scan(self,x,y,d,side):
         dx,dy=d
         L=1
@@ -99,61 +140,87 @@ class Board:
             return L-1
         else:
             return 0
-    def ScannotPlace(self,x,y,d,L,side):
+    def ScanPlace(self,x,y,d,L,side):
         if L>0:
             dx,dy=d
             for i in range(L):
                 self.setTile(x+dx*(i+1),y+dy*(i+1),side)
-    def Search(self,side):
-        a=0,0,0,0
+    def Search(self,side,showPossible=False):
+        a=[(0,0,0,0)]
+        m=0
         for iy in range(8):
             for ix in range(8):
                 b=self.ValidPlacement(ix,iy,side)
-                if b[0]>a[0]:
-                    a=b
-        return a
-    def Count(self):
+                if b[0]>m:
+                    a.clear()
+                    m=b[0]
+                if b[0]==m:
+                    a.append(b)
+                if b[0]>0 and showPossible:
+                    self.canvas.place(ix,iy,side,3)
+        import random
+        return random.choice(a)
+    def Count(self,redraw=False):
         out=[0,0,0]
         for iy in range(8):
             for ix in range(8):
-                out[self.getTile(ix,iy)]+=1
+                a=self.getTile(ix,iy)
+                out[a]+=1
+                if redraw and a!=0:
+                    self.canvas.place(ix,iy,a,0)
         return out
 
 
-
-            
-
-
 class Draw:
-    def __init__(self):
-        self.canvas=pygame.display.set_mode((800,800))
+    def __init__(self,size=(600,600)):
+        self.dimensions=(8,8)
+        self.size=size
+        self.margins=(50,50,50,50)
+        self.canvas=pygame.display.set_mode((self.size[0]+self.margins[0]+self.margins[2],self.size[1]+self.margins[1]+self.margins[3]))
     def Clear(self):
-        self.canvas.fill((0,0,0))
+        self.canvas.fill((0,50,0))
     def Update(self):
         pygame.display.update()
-    def Grid(self):
-        col=(100,100,100)
-        wid=5
-        for i in range(7):
-            pygame.draw.line(self.canvas,col,(0,(1+i)*100),(800,(1+i)*100),wid)
-        for i in range(7):
-            pygame.draw.line(self.canvas,col,((1+i)*100,0),((1+i)*100,800),wid)
-    def place(self,x,y,side):
-        col=(100,200,100*side)
-        pygame.draw.circle(self.canvas,col,(x*100+50,y*100+50),40)
+    def Grid(self,side=0):
+        if side==0:
+            col=(100,100,100)
+        else:
+            col=(side%2*200,side%2*200,side%2*200)
+        if side!=7:
+            wid=5
+            for i in range(9):
+                pygame.draw.line(self.canvas,col,(self.margins[0],self.margins[1]+(i)*self.size[1]//self.dimensions[1]),(self.size[0]+self.margins[0],self.margins[1]+(i)*self.size[1]//self.dimensions[1]),wid)
+            for i in range(9):
+                pygame.draw.line(self.canvas,col,(self.margins[0]+(i)*self.size[0]//self.dimensions[0],self.margins[1]),(self.margins[0]+(i)*self.size[0]//self.dimensions[0],self.size[1]+self.margins[1]),wid)
+    def place(self,x,y,side,t=0):
+        f=(0.4,0.2,0.15,0.1)[t]
+        col=(side%2*200,side%2*200,side%2*200)
+        s=int(f*self.size[0]/self.dimensions[0])
+        pygame.draw.circle(self.canvas,col,(int(self.margins[0]+(x+0.5)*self.size[0]/self.dimensions[0]),int(self.margins[1]+(y+0.5)*self.size[1]/self.dimensions[1])),s)
+
+    def getTile(self,point):
+        a=int((point[0]-self.margins[0])/(self.size[0]/self.dimensions[0]))
+        b=int((point[1]-self.margins[1])/(self.size[1]/self.dimensions[1]))
+        return min(a,7),min(b,7)
+
+def MainLoop(board):
+    run=True
+    while run:
+        pygame.time.delay(100)
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                run=False
+            if event.type==4:
+                a=event.__dict__['pos']
+                b=board.canvas.getTile(a)
+                board.x=b[0]
+                board.y=b[1]
+                board.updated=True
+            if event.type==5 and event.__dict__['button']==1:
+                board.pressed=True
+        board.Update()
 
 
-
-MyBoard=Board()
-run=True
-while run:
-    pygame.time.delay(300)
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            run=False
-        if event.type==5:
-            a=event.__dict__['pos']
-            MyBoard.x=int(a[0]/100)
-            MyBoard.y=int(a[1]/100)
-            MyBoard.pressed=True
-    MyBoard.Update()
+if __name__ == "__main__":
+    MyBoard=Board((0,0),1,1)
+    MainLoop(MyBoard)
